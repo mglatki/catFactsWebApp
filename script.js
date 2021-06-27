342622ca4e9459576fe095093cb0071e6e5b4c39
Michał <mglatki@gmail.com>
2021-06-27 09:01:22 +0200

    Basic facts items functions

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+let factsGridItems = new Array();
+
+const createFactsGridItem = function (itemType, content) {
+    const item = {
+        itemType: itemType,
+        content: content,
+    }
+
+    factsGridItems.push(item);
+}
+
+const createInfoItem = function () {
+    createFactsGridItem(`info`,`In this grid you can dosnload and view fn and informative facts about cats`);
+}
