b06abc038efedde0578d5a906c3e7f7a171a92ef
mglatki <mglatki@gmail.com>
2021-06-29 20:11:11 +0200

    Fun with Prettier

--- script.js
+++ script.js
@@ -1,82 +1,81 @@
-'use strict';
+"use strict";
 
 let factsGridItems = new Array();
 const grid = document.querySelector(`main`);
 
 const createFactsGridItem = function (itemType, content) {
-    const item = {
-        itemType: itemType,
-        content: content,
-    }
+  const item = {
+    itemType: itemType,
+    content: content,
+  };
 
-    if(itemType === `fact`) {
-        factsGridItems.splice((factsGridItems.length - 1), 0, item);
-    }
-    else
-    factsGridItems.push(item);
-}
+  if (itemType === `fact`) {
+    factsGridItems.splice(factsGridItems.length - 1, 0, item);
+  } else factsGridItems.push(item);
+};
 
 const createInfoItem = function () {
-    createFactsGridItem(`info`,`In this grid you can download and view fn and informative facts about cats`);
-}
+  createFactsGridItem(
+    `info`,
+    `In this grid you can download and view informative facts about cats`
+  );
+};
 
 const createAddItemButton = function () {
-    createFactsGridItem(`add`,``)
-}
+  createFactsGridItem(`add`, ``);
+};
 
 const getCatFact = async () => {
-    try {
-        const url = `https://catfact.ninja/fact`;
-        const res = await fetch(url);
-        console.log(res.ok);
-        const data = await res.json();
-        console.log(data);
-        return data;
-    } catch (error) {
-        console.log(error);
-        return error;
-    }
-    }
+  try {
+    const url = `https://catfact.ninja/fact`;
+    const res = await fetch(url);
+    console.log(res.ok);
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
 
 const createCatFactItem = async () => {
-    const data = await getCatFact();
-    createFactsGridItem(`fact`,data.fact)
-    createFactsGrid();
-}
+  const data = await getCatFact();
+  createFactsGridItem(`fact`, data.fact);
+  createFactsGrid();
+};
 
 const addFactItemToGrid = function (factItem) {
-
-    let factItemHTML = ``;
-    if (factItem.itemType === `add`) {
-        factItemHTML = `
+  let factItemHTML = ``;
+  if (factItem.itemType === `add`) {
+    factItemHTML = `
         <div class="factItem">
             <button class="addFactButton">+</button>
         </div>`;
 
-        grid.innerHTML += factItemHTML;
+    grid.innerHTML += factItemHTML;
 
-        document
-        .querySelector(`.addFactButton`)
-        .addEventListener(`click`, createCatFactItem);
-    } else {
-        factItemHTML = `
+    document
+      .querySelector(`.addFactButton`)
+      .addEventListener(`click`, createCatFactItem);
+  } else {
+    factItemHTML = `
         <div class="factItem">
             <p class="factContent">${factItem.content}</p>
         </div>`;
 
-        grid.innerHTML += factItemHTML;
-    }
-}
+    grid.innerHTML += factItemHTML;
+  }
+};
 
 const createFactsGrid = function () {
-    grid.innerHTML = ``;
+  grid.innerHTML = ``;
 
-    factsGridItems.forEach(item => {
-        addFactItemToGrid(item);
-    });
-}
+  factsGridItems.forEach((item) => {
+    addFactItemToGrid(item);
+  });
+};
 
 createInfoItem();
 createAddItemButton();
 createFactsGrid();
-
